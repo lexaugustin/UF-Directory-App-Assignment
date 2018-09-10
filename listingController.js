7582c112cf4f9b79711a26e6569451ccf76e436f
Alexander Augustin <alexander@Alexanders-MacBook-Pro.local>
2018-09-10 18:16:50 -0400

    Initial submission

--- listingController.js
+++ listingController.js
@@ -1,14 +1,36 @@
-angular.module('listings').controller('ListingsController', ['$scope', 'Listings', 
+angular.module('listings').controller('ListingsController', ['$scope', 'Listings',
   function($scope, Listings) {
     $scope.listings = Listings;
     $scope.detailedInfo = undefined;
 
-    /* 
-      Implement these functions in the controller to make your application function 
-      as described in the assignment spec. 
+    /*
+      Implement these functions in the controller to make your application function
+      as described in the assignment spec.
      */
-    $scope.addListing = function() {};
-    $scope.deleteListing = function(index) {};
-    $scope.showDetails = function(index) {};
+    $scope.addListing = function() {
+      $scope.listings.push({
+        "code": $scope.newListing.code,
+        "name": $scope.newListing.name,
+        "latitude": $scope.newListing.coordinates.latitude,
+        "longitude": $scope.newListing.coordinates.longitude,
+        "address": $scope.newListing.address
+      });
+
+      $scope.newListing.code = "";
+      $scope.newListing.name = "";
+      $scope.newListing.coordinates.latitude = "";
+      $scope.newListing.coordinates.longitude = "";
+      $scope.newListing.address = "";
+    };
+
+
+    $scope.deleteListing = function(index) {
+      var removedListing = $scope.listings.indexOf(index);
+      $scope.listings.splice(removedListing, 1);
+    };
+
+    $scope.showDetails = function(index) {
+      $scope.selectedIndex = index;
+    };
   }
 ]);
